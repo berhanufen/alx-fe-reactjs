101510462f698eec8ff062dabf6fdb2fbd56fa60
Fenet Berhanu <berhanufen@gmail.com>
2026-01-18 10:15:47 -0500

    Add WelcomeMessage JSX component

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
+import WelcomeMessage from './components/WelcomeMessage';
 
 function App() {
   return (
     <>
       <Header />
+      <WelcomeMessage />
       <MainContent />
       <Footer />
     </>
