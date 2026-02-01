c3a36b42ba79ef68b8cddc9179bc6353ff71c3a1
edlawit sewasew <edlawitsewasew90@gmail.com>
2026-02-01 22:27:18 +0300

    add recipe-sharing-app

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,15 @@
+import RecipeList from './components/RecipeList'
+import AddRecipeForm from './components/AddRecipeForm'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Recipe Sharing App</h1>
+      <AddRecipeForm />
+      <RecipeList />
+    </>
+  )
+}
+
+export default App
