bbee4f4a81f799b2cf284cc7692c111e9bcd6499
edlawit sewasew <edlawitsewasew90@gmail.com>
2026-02-02 10:52:01 +0300

    update

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import UserProfile from './components/UserProfile'
 import './App.css'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
 
   return (
     <>
+      <UserProfile />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
